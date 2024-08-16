6f0bf9291288fa5aca6d703e4fb38e466e79cac9
alessandragaleno <alessandragaleno421@gmail.com>
2024-08-16 20:02:14 -0300

    atualização de cadastro

--- cadastro_de_usuario/lib/main.dart
+++ cadastro_de_usuario/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tela de Cadastro',
       theme: ThemeData(
-       primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
       home: SignupScreen(),
     );
@@ -23,7 +23,7 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  final _formkey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -31,7 +31,6 @@ class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _cpfController = TextEditingController();
 
-// continua da aplicação
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,67 +38,107 @@ class _SignupScreenState extends State<SignupScreen> {
         title: Text('Cadastro de Usuário'),
       ),
       body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Form(
-          key: _formkey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                      labelText: 'Nome de Usuário',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, insira um nome de usuário';
-                      }
-                      return null;
-                    },
-                  )
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Nome de Usuário',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira um nome de usuário';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-            SizedBox(height: 16),
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-                border: OutlineInputBorder(),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira um e-mail';
+                    } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                      return 'Por favor, insira um e-mail válido';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _cpfController,
+                  decoration: InputDecoration(
+                    labelText: 'CPF',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira seu CPF';
+                    } else if (!RegExp(r'^[0-9]{11}$').hasMatch(value)) {
+                      return 'Por favor, insira um CPF válido';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira uma senha';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  decoration: InputDecoration(
+                    labelText: 'Confirmação de Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'As senhas não coincidem';
+                    } else if (value != _passwordController.text) {
+                      return 'As senhas não coincidem';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Ação a ser tomada ao clicar no botão, como enviar os dados para um servidor!
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Cadastrando...')),
+                      );
+                    }
+                  },
+                  child: Text('Cadastrar'),
+                ),
+              ],
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Por favor, insira um e-mail';
-                }
-                else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                return 'Porfavor, insira um e-mail válido';
-                }
-                return null;
-            }
-            SizedBox(height: 16),
-            TextFormField(
-              controller: _cpfController,
-              decoration: InputDecoration(
-                labelText: 'CPF',
-                border: OutlineInputBorder(),
-            ),
-            keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Por favor, insira seu CPF';
-                }
-                else if (!RegExp(r'^[^0-9](^11)$').hasMatch(value)) {
-                return 'Porfavor, insira seu CPF válido';
-                }
-                return null;
-            }
-          ],
+          ),
         ),
       ),
-    ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
   }
 }
